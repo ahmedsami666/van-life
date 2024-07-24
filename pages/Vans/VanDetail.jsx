382a30ae6f58304044ccc09c69ed8fe2c1f842af
AhmedSami <71245088+ahmedsami666@users.noreply.github.com>
2024-07-24 23:14:43 +0300

    adding 404 page

--- pages/Vans/VanDetail.jsx
+++ pages/Vans/VanDetail.jsx
@@ -1,8 +1,11 @@
 import React from "react"
-import { Link, useParams } from "react-router-dom"
+import { Link, useParams, useLocation } from "react-router-dom"
 
 export default function VanDetail() {
     const params = useParams()
+    const location = useLocation()
+    console.log(location)
+    
     const [van, setVan] = React.useState(null)
 
     React.useEffect(() => {
@@ -10,14 +13,17 @@ export default function VanDetail() {
             .then(res => res.json())
             .then(data => setVan(data.vans))
     }, [params.id])
-
+    
+    const search = location.state?.search || "" // equals to => const search = location.state && location.state.search || ""
+    const type = location.state?.type || "all"
+    
     return (
         <div className="van-detail-container">
             <Link
-                to=".."
+                to={`..${search}`}
                 relative="path"
                 className="back-button"
-            >&larr; <span>Back to all vans</span></Link>
+            >&larr; <span>Back to {type} vans</span></Link>
             
             {van ? (
                 <div className="van-detail">
